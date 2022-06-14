dec3326b53e0a26b2f0cbbe5f3d3ddc8c3bbc34a
mmiesch <mark.s.miesch@gmail.com>
2022-06-14 13:28:53 -0600

    pass file list to idl program

---
+++ combine_stereo_pb.pro
@@ -0,0 +1,5 @@
+pro combine_stereo_pb, filelist
+
+print,"In combine_stereo_pb: ", filelist
+
+end
--- stereo_process_pB.py
+++ stereo_process_pB.py
@@ -7,8 +7,25 @@ import numpy as numpy
 import os
 import subprocess
 
-message = "hello"
+#------------------------------------------------------------------------------
+# define file list
+
+pb_dir = 'data/seq/'
+
+pb_files = ['20220601_040835_n7c2A.fts', \
+            '20220601_040905_n7c2A.fts', \
+            '20220601_040935_n7c2A.fts'
+]
+
+filelist = ""
+
+for f in pb_files:
+    filelist += pb_dir+f
+    if f != pb_files[-1]:
+        filelist += ','
+
+#------------------------------------------------------------------------------
 
 sswidl = "/usr/local/ssw/gen/setup/ssw_idl"
 
-subprocess.run([sswidl,"-e",f'print,\"{message}\"'], env=os.environ)
+subprocess.run([sswidl,"-e",f"combine_stereo_pb,'{filelist}'"], env=os.environ)
