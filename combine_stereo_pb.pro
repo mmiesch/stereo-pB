e2bdd554481727da1e68dd29aa4b3aaa6377a5e3
mmiesch <mark.s.miesch@gmail.com>
2022-06-14 17:51:09 -0600

    latest

--- combine_stereo_pb.pro
+++ combine_stereo_pb.pro
@@ -7,4 +7,14 @@ print,file3
 
 secchi_prep, [file1,file2,file3], h, a, /smask
 
+times = [h[0].date, h[1].date, h[2].date]
+image = (2./3.d0)*total(a,3)
+
+print,h[0].date
+help,h[0].date
+
+;h = h[1]
+;h.date = date
+;sccwritefits, outfilename, image, h
+
 end
--- stereo_process_pB.py
+++ stereo_process_pB.py
@@ -7,6 +7,9 @@ import numpy as numpy
 import os
 import subprocess
 
+from astropy.io import fits
+from astropy.time import Time
+
 #------------------------------------------------------------------------------
 # define file list
 
@@ -18,9 +21,35 @@ f = ['20220601_040835_n7c2A.fts', \
 ]
 
 #------------------------------------------------------------------------------
+# read metadata
+
+print(80*'-')
+
+times = []
+pol = []
+
+for file in f:
+    hdu = fits.open(dir+file)[0]
+    times.append(Time(hdu.header['DATE']))
+    pol.append(hdu.header['POLAR'])
+
+
+for t in times:
+    print(t)
+
+for p in pol:
+    print(f"pb file polarization {p}")
+
+
+
+#------------------------------------------------------------------------------
+# define a representative time and output file name
+
+
+#------------------------------------------------------------------------------
 
 sswidl = "/usr/local/ssw/gen/setup/ssw_idl"
 
 idlcommand = f"combine_stereo_pb,'{dir+f[0]}','{dir+f[1]}','{dir+f[2]}'"
 
-subprocess.run([sswidl,"-e",idlcommand], env=os.environ)
+#subprocess.run([sswidl,"-e",idlcommand], env=os.environ)
