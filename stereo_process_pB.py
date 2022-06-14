1720e9ad09a1689860356eafb0f19ba5ec36bdf6
mmiesch <mark.s.miesch@gmail.com>
2022-06-14 11:22:13 -0600

    simple subprocess call

---
+++ stereo_process_pB.py
@@ -0,0 +1,13 @@
+"""
+Script for processing polarized images from STEREO-A such that they can be used by the SWPC CME Analysis Tool (CAT)
+"""
+
+
+import numpy as numpy
+import subprocess
+
+message = "hello"
+
+subprocess.run(["bash", "-c", f"echo {message}"])
+
+
