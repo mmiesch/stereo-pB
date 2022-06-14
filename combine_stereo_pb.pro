794e003103d977bf9989ca86cf626250e6032947
mmiesch <mark.s.miesch@gmail.com>
2022-06-14 13:42:40 -0600

    secchi_prep runs from python

--- combine_stereo_pb.pro
+++ combine_stereo_pb.pro
@@ -1,5 +1,10 @@
-pro combine_stereo_pb, filelist
+pro combine_stereo_pb, file1, file2, file3
 
-print,"In combine_stereo_pb: ", filelist
+print,"In combine_stereo_pb: "
+print,file1
+print,file2
+print,file3
+
+secchi_prep, [file1,file2,file3], h, a, /smask
 
 end
--- stereo_process_pB.py
+++ stereo_process_pB.py
@@ -10,22 +10,17 @@ import subprocess
 #------------------------------------------------------------------------------
 # define file list
 
-pb_dir = 'data/seq/'
+dir = 'data/seq/'
 
-pb_files = ['20220601_040835_n7c2A.fts', \
-            '20220601_040905_n7c2A.fts', \
-            '20220601_040935_n7c2A.fts'
+f = ['20220601_040835_n7c2A.fts', \
+     '20220601_040905_n7c2A.fts', \
+     '20220601_040935_n7c2A.fts'
 ]
-
-filelist = ""
-
-for f in pb_files:
-    filelist += pb_dir+f
-    if f != pb_files[-1]:
-        filelist += ','
 
 #------------------------------------------------------------------------------
 
 sswidl = "/usr/local/ssw/gen/setup/ssw_idl"
 
-subprocess.run([sswidl,"-e",f"combine_stereo_pb,'{filelist}'"], env=os.environ)
+idlcommand = f"combine_stereo_pb,'{dir+f[0]}','{dir+f[1]}','{dir+f[2]}'"
+
+subprocess.run([sswidl,"-e",idlcommand], env=os.environ)
