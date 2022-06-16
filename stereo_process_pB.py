468794cf417251c5a5a5edff7d4978d891de4cb9
mmiesch <mark.s.miesch@gmail.com>
2022-06-16 17:38:54 -0600

    implemented time threshold

--- stereo_process_pB.py
+++ stereo_process_pB.py
@@ -36,16 +36,17 @@ for file in f:
     times.append(Time(hdu.header['DATE']).gps)
     pol.append(hdu.header['POLAR'])
 
-
 #------------------------------------------------------------------------------
 # define an output file name based on the average time stamp
 
 t = np.array(times)
 
+dt = np.max(t) - np.min(t)
+
 time = Time(0.5*(np.min(t) + np.max(t)), format = 'gps')
 time.format='iso'
 
-print(f"time stamp = {time}")
+print(f"time stamp, dt = {time} {dt}")
 
 outfile = time.strftime('%Y%m%d_%H%M%S'+'_pBcom.fts')
 
@@ -53,21 +54,26 @@ print(f"outfile = {outfile}")
 
 #------------------------------------------------------------------------------
 # check for a matching set of polarizations
+# and check that all the measurements are within 30 min of each other
 # if a match, then run the procedure
 
 set = [0.0, 120.0, 240.0]
+
+# max allowable time difference, in seconds
+dtmax = 1800.
+
 sswidl = "/usr/local/ssw/gen/setup/ssw_idl"
 
 for p in pol:
-    print(f"pb file polarization {p} {type(p)}")
+    print(f"pb file polarization {p}")
 
-if all(x in pol for x in set):
-    print("complete set: creating pB composit file")
+if all(x in pol for x in set) and dt < dtmax:
+    print("complete set: creating pB composite file")
     idlcommand = f"combine_stereo_pb,'{f[0]}','{f[1]}','{f[2]}'"
     subprocess.run([sswidl,"-e",idlcommand], env=os.environ)
 
 else:
-    print("incomplete set: exiting")
+    print(f"incomplete set: exiting {pol} {dt/60}")
 
 #------------------------------------------------------------------------------
 
