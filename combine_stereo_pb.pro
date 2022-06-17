cb207729ed9b78eb5f3c3b534eda1d6a2d046bbd
mmiesch <mark.s.miesch@gmail.com>
2022-06-16 18:11:45 -0600

    idl time conversion seems to be working

--- combine_stereo_pb.pro
+++ combine_stereo_pb.pro
@@ -1,4 +1,8 @@
-pro combine_stereo_pb, file1, file2, file3
+pro combine_stereo_pb, file1, file2, file3, time, outfile
+
+; the time should be passed as a string in iso format for
+; proper inclusion in the header, e.g.
+; '2022-06-14T21:14:54.040'
 
 print,"In combine_stereo_pb: "
 print,file1
@@ -7,14 +11,29 @@ print,file3
 
 secchi_prep, [file1,file2,file3], h, a, /smask
 
-times = [h[0].date, h[1].date, h[2].date]
+h = h[1]
+
+;times = [h[0].date, h[1].date, h[2].date]
 image = (2./3.d0)*total(a,3)
 
-print,h[0].date
-help,h[0].date
+mjd = time - 2400000.5d
+mjd_str={ mjd: floor(mjd), time: (mjd-floor(mjd))*8.64d7 }
+;h.date = anytim2utc(/ccds,mjd_str)
 
-;h = h[1]
-;h.date = date
-;sccwritefits, outfilename, image, h
+;print,anytim2utc(/ccds,mjd_str)
+
+print,"AAAAAA "
+jd=systime(/julian,/utc)
+help,jd
+help,time
+;mjd=jd-2400000.5d
+mjd=double(time)-2400000.5d
+mjd_str={ mjd: floor(mjd), time: (mjd-floor(mjd))*8.64d7 }
+print,anytim2utc(/ccsds,mjd_str)
+
+;print, h.date
+
+
+;sccwritefits, outfile, image, h
 
 end
--- stereo_process_pB.py
+++ stereo_process_pB.py
@@ -11,9 +11,19 @@ from astropy.io import fits
 from astropy.time import Time
 
 #------------------------------------------------------------------------------
-# retrieve the most recent three files in selected directory.
+# define system-specific paramters
 
+# input directory
 dir = 'data/seq/'
+
+# output directory
+outdir = 'data/pBcom/'
+
+# location of sswidl executable
+sswidl = "/usr/local/ssw/gen/setup/ssw_idl"
+
+#------------------------------------------------------------------------------
+# retrieve the most recent three files in selected directory.
 
 files = list(filter(os.path.isfile, glob.glob(dir + "*.fts")))
 files.sort(key=lambda x: os.path.getmtime(x))
@@ -48,7 +58,7 @@ time.format='iso'
 
 print(f"time stamp, dt = {time} {dt}")
 
-outfile = time.strftime('%Y%m%d_%H%M%S'+'_pBcom.fts')
+outfile = outdir + time.strftime('%Y%m%d_%H%M%S'+'_pBcom.fts')
 
 print(f"outfile = {outfile}")
 
@@ -62,14 +72,12 @@ set = [0.0, 120.0, 240.0]
 # max allowable time difference, in seconds
 dtmax = 1800.
 
-sswidl = "/usr/local/ssw/gen/setup/ssw_idl"
-
 for p in pol:
     print(f"pb file polarization {p}")
 
 if all(x in pol for x in set) and dt < dtmax:
     print("complete set: creating pB composite file")
-    idlcommand = f"combine_stereo_pb,'{f[0]}','{f[1]}','{f[2]}'"
+    idlcommand = f"combine_stereo_pb,'{f[0]}','{f[1]}','{f[2]}','{time.jd}','{outfile}'"
     subprocess.run([sswidl,"-e",idlcommand], env=os.environ)
 
 else:
