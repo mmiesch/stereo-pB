46d9fb4e4d3221cb22dae732ced593adb6139807
mmiesch <mark.s.miesch@gmail.com>
2022-06-17 07:09:48 -0600

    add process_dir.py

---
+++ RT/process_dir.py
@@ -0,0 +1,26 @@
+
+from astropy.time import Time
+from stereo_process_pB import process_files
+
+"""
+This function is for processing STEREO-A polarized brightness (pB) file in "batch" mode.  It processes all files in a specified target directory.  It starts by generating a file list for the target directory, then it loops through all files in the directory looking for two other files that form a complete set.
+
+A complete set consists of three observations with different polarization states (0, 120, and 240) all taken within 30 minutes of one another.  If a complete set is found, the three pB images are processed with the SolarSoft `secchi_prep` function and the images are combined to form a total brightness (tB) image.   This tB image is written as a fits file to a specified output directory.  The output file name reflects the date of the observation and includes the tag `pBcom` to represent a polarized brightness composite.
+
+This function is used to process all files in the target directory.  So, if files come in one at a time throughout the course of a day and are written to the target directory, then this function will do unneccesary work.  It will process all the earlier files again.
+
+If the pB (seq) files arrive one at a time then the `watch_dir.py` function should be used instead of this one.  It is expected that `watch_dir.py` will be the primary function for operational use.  This function is provided mainly as a utility, for manual operation in case something goes wrong with the nominal operational process.
+
+"""
+#------------------------------------------------------------------------------
+# define platform-specific parameters
+
+# this is the directory to monitor for new files
+#dir = '../data/seq'
+targetdir = '../data/tmp/'
+
+#output directory
+outdir = '../data/pBcom/'
+
+# location of sswidl executable
+sswidl = "/usr/local/ssw/gen/setup/ssw_idl"
---
+++ RT/watch_dir.py
@@ -0,0 +1,52 @@
+
+import time
+from stereo_process_pB import process_files
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+"""
+This function is for processing STEREO-A polarized brightness (pB) file in real-time "watch" mode.  The function runs continuously, monitoring a specified target directory for the appearance of a new file.  When a new fits file appears, the function will group this file with the previous two files in terms of the date of file creation.  Then it checks to see if these three files form a complete set.  
+
+A complete set consists of three observations with different polarization states (0, 120, and 240) all taken within 30 minutes of one another.  If a complete set is found, the three pB images are processed with the SolarSoft `secchi_prep` function and the images are combined to form a total brightness (tB) image.   This tB image is written as a fits file to a specified output directory.  The output file name reflects the date of the observation and includes the tag `pBcom` to represent a polarized brightness composite.
+
+This function only works if the pB (seq) files arrive one at a time, with an intervening delay of at least 20 seconds.  If more than three files are added to the monitored directory simultaneously, then it will miss some files that should be processed.
+
+If more than three files are added to the target directory at one time, then it is recommended to use the `process_dir.py` function instead of this one.
+
+"""
+#------------------------------------------------------------------------------
+# define platform-specific parameters
+
+# this is the directory to monitor for new files
+#dir = '../data/seq'
+targetdir = '../data/tmp/'
+
+#output directory
+outdir = '../data/pBcom/'
+
+# location of sswidl executable
+sswidl = "/usr/local/ssw/gen/setup/ssw_idl"
+
+#------------------------------------------------------------------------------
+# event handler
+
+class Handler(FileSystemEventHandler):
+    def on_created(self, event):
+        print("New image found: ", event.src_path)
+        process_files(targetdir, outdir, sswidl)
+
+#------------------------------------------------------------------------------
+# main program that runs continually
+
+if __name__ == "__main__":
+    event_handler = Handler()
+    observer = Observer()
+    observer.schedule(event_handler, dir, recursive=False)
+    observer.start()
+    print("Monitoring for files....")
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
